934ad36214245a5b5d38855f7278d1486138729f
ibrahiM10 <ibrahim.sma1206@gmail.com>
2025-09-05 15:57:58 -0400

    Agregado contexto para manejar inicio de sesión de usuario.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginScreen from './components/LoginScreen/LoginScreen';
-import DashboardScreen from './components/DashboardScreen/DashboardScreen';
-import HiveDetailScreen from './components/HiveDetailScreen/HiveDetailScreen';
-import ReportsScreen from './components/ReportsScreen/ReportsScreen';
-import SettingsScreen from './components/SettingsScreen/SettingsScreen';
-import HiveManagementScreen from './components/HiveManagementScreen/HiveManagementScreen'; // <-- Nueva importación
-import WelcomeScreen from './components/WelcomeScreen/WelcomeScreen'; 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import LoginScreen from "./components/LoginScreen/LoginScreen";
+import DashboardScreen from "./components/DashboardScreen/DashboardScreen";
+import HiveDetailScreen from "./components/HiveDetailScreen/HiveDetailScreen";
+import ReportsScreen from "./components/ReportsScreen/ReportsScreen";
+import SettingsScreen from "./components/SettingsScreen/SettingsScreen";
+import HiveManagementScreen from "./components/HiveManagementScreen/HiveManagementScreen"; // <-- Nueva importación
+import WelcomeScreen from "./components/WelcomeScreen/WelcomeScreen";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<WelcomeScreen />} />
-                    <Route path="/login" element={<LoginScreen />} />
-                    <Route path="/dashboard" element={<DashboardScreen />} />
-                    <Route path="/colmena/:hiveId" element={<HiveDetailScreen />} />
-                    <Route path="/reports" element={<ReportsScreen />} />
-                    <Route path="/settings" element={<SettingsScreen />} />
-                    <Route path="/hives" element={<HiveManagementScreen />} /> {/* <-- Nueva ruta */}
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<WelcomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/dashboard" element={<DashboardScreen />} />
+            <Route path="/colmena/:hiveId" element={<HiveDetailScreen />} />
+            <Route path="/reports" element={<ReportsScreen />} />
+            <Route path="/settings" element={<SettingsScreen />} />
+            <Route path="/hives" element={<HiveManagementScreen />} />{" "}
+            {/* <-- Nueva ruta */}
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
